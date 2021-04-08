81311d7095bf936b352e2d6f1af9e8a94bf8b775
John Morgan <johnhmorgan@outlook.com>
2021-04-08 11:23:04 +0100

    managing variable url for Pokemon

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -17,13 +17,13 @@ function CardTable() {
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={getData}>Add a playing card!</button>
+        <button onClick={() => getData() }>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
   {cards.map(cardData => (
     <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
   ))}
-</div>
+      </div>
     </div>
   );
 }
--- src/useAxios.js
+++ src/useAxios.js
@@ -2,19 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import { v4 as uuid} from 'uuid';
 
-
-
 const useAxios = (url) => {
     const [cards, setCards] = useState([]);
-  
-    //it is here I have to add on to the base url
-    //where do I get the rest of the url from?
     const getData = async (name) => {    
-        if(name){
-            url = url + name
-        }
-        else
-        {url = url}
+        if(name){url = url + name}
         console.log("name is: ", name);
         console.log("url is: ", url)
         const response = await axios.get(url);
